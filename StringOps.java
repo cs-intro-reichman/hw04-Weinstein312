6c388f3b21343c5091e7032643d6ea740d4ff2e8
Weinstein312 <jacob.weinstein@runi.ac.il>
2024-01-15 22:36:46 +0200

    update 3

--- StringOps.java
+++ StringOps.java
@@ -22,17 +22,90 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(camelCase("Intro To COmputer Science Rules"));
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String newString = "";
+        for (int i = 0; i < string.length(); i++) {
+            char ichar = string.charAt(i);
+
+            // if ichar is a letter A-Z (uppercase)
+            if ((ichar >= 65) && (ichar <= 90)) {
+                if (isVowel(ichar)) { // if uppercase vowel, simply add the char
+                    newString += ichar;
+                }
+                else { // if uppercase non-vowel, make it lowercase
+                    newString += (char) (ichar + 32);
+                }
+            }
+            // if ichar is a letter a-z (lowercase)
+            else if ((ichar >= 97) && (ichar <= 122)) {
+                if (isVowel(ichar)) { // if lowercase vowel, make it uppercase
+                    newString += (char) (ichar - 32);
+                }
+                else { // if lowercase non-vowel, simply add the char
+                    newString += ichar;
+                }
+            }
+            else { // if not a letter, simply add the char
+                newString += ichar;
+            }
+
+        }
+        return newString;
+    }
+
+    public static boolean isVowel (char c) {
+        boolean vowel = false;
+
+        if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u')) {
+            vowel = true;
+        }
+
+        if ((c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U')) {
+            vowel = true;
+        }
+
+        return vowel;
     }
 
     public static String camelCase (String string) {
         // Write your code here:
-        return "";
+        String newString = "";
+        boolean spaceAbove = false;
+        for (int i = 0; i < string.length(); i++) {
+            
+            // default spaceAbove to false, make it true if the prior char is space
+            spaceAbove = false;
+            if ((i > 0) && (string.charAt(i-1) == ' ')) {
+                spaceAbove = true;
+            }
+
+            char ichar = string.charAt(i);
+
+            if ((ichar >= 65) && (ichar <= 90)) { // Uppercase
+                if (spaceAbove == true) {
+                    newString += ichar;
+                }
+                else {
+                    newString += (char) (ichar + 32);
+                }
+            }
+            else if ((ichar >= 97) && (ichar <= 122)) { // Lowercase
+                if (spaceAbove == true) {
+                    newString += (char) (ichar - 32);
+                }
+                else {
+                    newString += ichar;
+                }
+            }
+
+            if (ichar == ' ') {
+                spaceAbove = true;
+            }
+        }
+        return newString;
     }
 
     public static int[] allIndexOf (String string, char chr) {
